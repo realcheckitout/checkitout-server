f363ec491426abb3a5ac2b3c74c05fd7b949120e
Woojin <wj05132@gmail.com>
2024-08-03 03:52:06 +0900

    initial commit

---
+++ src/main/java/threetwoone/jocoding/JocodingApplication.java
@@ -0,0 +1,13 @@
+package threetwoone.jocoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JocodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JocodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/threetwoone/jocoding/JocodingApplicationTests.java
@@ -0,0 +1,13 @@
+package threetwoone.jocoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JocodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
